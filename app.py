d4151738bc3ff7b06534bc70743675390e5c0d5e
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-11-20 14:30:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, url_for, request, session, redirect, f
 from flask.ext.pymongo import PyMongo
 import bcrypt
 import time
+from bson.objectid import ObjectId
+import hashlib
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'blog'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/blog'  # for local db
 app.config['MONGO_URI'] = 'mongodb://dushyant7917:abc123@ds019471.mlab.com:19471/blog'
 # app.config['MONGO_URI'] = 'mongodb://localhost:27017/blog' //for local db
 
@@ -36,15 +39,70 @@ def blog():
         return "dff"
 
 
+@app.route('/comment/<article_id>', methods = ['POST'])
+def comment(article_id):
+    if request.method == 'POST':
+        if 'username' in session:
+            new_comment = request.form['comment']
 
-@app.route('/comment/<string:artcl>')
-def comment(artcl):
+            articles = mongo.db.articles
+            item = articles.find_one({'_id': ObjectId(article_id)})
+            hash_str = item['date'] + item['author'] + item['title']
+            art_id = hashlib.md5(hash_str).hexdigest()
+            date = time.strftime("%B %d,"+" %Y")
+            users = mongo.db.users
+            author = users.find_one({'username': item['author']})
+            comments = mongo.db.comments
+            comments.insert({'comment_id': art_id, 'comment': new_comment, 'comment_username': session['username'], 'date': date, 'pic': author['pic']})
+            #articles = mongo.db.articles
+
+            return redirect(url_for('article', article_id = article_id))
+
+        else:
+            render_template('login.html')
+
+    else:
+        return render_template('login.html')
+
+
+@app.route('/article/<article_id>')
+def article(article_id):
     if 'username' in session:
-        print artcl['author']
-        return render_template('comment.html', item = artcl)
+        articles = mongo.db.articles
+        item = articles.find_one({'_id': ObjectId(article_id)})
+        hash_str = item['date'] + item['author'] + item['title']
+        art_id = hashlib.md5(hash_str).hexdigest()
+        users = mongo.db.users
+        author = users.find_one({'username': item['author']})
+        comments = mongo.db.comments
+        comment_data = comments.find({'comment_id': art_id})
+        lC = []
+        lN = []
+        lD = []
+        lP = []
+        for i in comment_data:
+            lP.append(i['pic'])
+            lD.append(i['date'])
+            lC.append(i['comment'])
+            lN.append(i['comment_username'])
+        return render_template('article.html', item = item, author = author, comment_data = zip(lN,lC,lD,lP))
 
     return render_template('login.html')
 
+
+
+@app.route('/like/<article_id>', methods = ['GET'])
+def like(article_id):
+    if request.method == "GET":
+        articles = mongo.db.articles
+        item = articles.find_one({'_id': ObjectId(article_id)})
+        print item['likes']
+        likes = item['likes']
+        articles.update({'_id': ObjectId(article_id)}, {'$set': {'likes': likes + 1}})
+        return redirect(url_for('article', article_id = item['_id']))
+
+    else:
+        return "Error!"
 
 
 @app.route('/profile')
@@ -52,28 +110,39 @@ def profile():
     if 'username' in session:
         users = mongo.db.users
         details = users.find_one({'username': session['username']})
-        return render_template('profile.html', details = details)
+        articles = mongo.db.articles
+        blogs = articles.find({'author': session['username']})
+        tt = []
+        pc = []
+        dt = []
+        smmry = []
+        for i in blogs:
+            tt.append(i['title'])
+            dt.append(i['date'])
+            pc.append(i['pic'])
+        return render_template('profile.html', details = details, blogs = zip(pc,tt,dt))
 
     return render_template('login.html')
 
 
-@app.route('/editProfile', methods=['GET', 'POST'])
-def editProfile():
+@app.route('/editProfile/<username>', methods=['GET', 'POST'])
+def editProfile(username):
     if request.method == "GET":
         if 'username' in session:
             users = mongo.db.users
-            details = users.find_one({'username': session['username']})
+            details = users.find_one({'username': username})
             return render_template('editProfile.html', details = details)
 
         return render_template('login.html')
 
     else:
         name = request.form['name']
-        age = request.form['age']
+        about = request.form['about']
+        dob = request.form['dob']
         profession = request.form['profession']
         pic = request.form['pic']
         users = mongo.db.users
-        users.update({'username': session['username']}, {'$set': {'name': name, 'age': age, 'profession': profession, 'pic': pic}})
+        users.update({'username': session['username']}, {'$set': {'name': name, 'about': about, 'dob': dob, 'profession': profession, 'pic': pic}})
         return redirect(url_for('profile'))
 
 
@@ -86,11 +155,13 @@ def addBlog():
             return render_template('addBlog.html')
         else:
             articles = mongo.db.articles
+            tags = request.form['tags']
+            tags = tags.split(";")
             art = request.form['editor1']
             artTitle = request.form['title']
             artPic = request.form['art_pic']
             date = time.strftime("%B %d,"+" %Y")
-            articles.insert({ 'author': session['username'], 'article': art , 'title': artTitle , 'date': date, 'pic': artPic})
+            articles.insert({ 'author': session['username'], 'article': art , 'title': artTitle , 'date': date, 'pic': artPic, 'tags': tags})
             return redirect(url_for('blog'))
 
     else:
@@ -158,4 +229,3 @@ if __name__ == '__main__':
     app.secret_key = 'dushyant7917'
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
